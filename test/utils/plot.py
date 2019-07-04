b4b1bc6027daee098adf5fefc6f90ff165682781
Benoit Le Quéau <benoit.lequeau@slimcut.com>
2019-07-04 08:17:26 +0200

    [Utils] added plot function

---
+++ test/utils/plot.py
@@ -0,0 +1,16 @@
+import matplotlib as plt
+
+
+def plot_roc(fpr, tpr, auc):
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
